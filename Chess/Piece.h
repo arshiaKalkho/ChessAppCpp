a33a03a9815402aa3cbb29a36088b9f6ac10bb8e
arshia <arshiabdol@yahoo.com>
2021-09-08 05:02:11 -0400

    added Square classes

--- Chess/Piece.h
+++ Chess/Piece.h
@@ -10,21 +10,28 @@ class Piece
 {
 public:
 	string name = "";
-	bool isCaptured;
-	
+	//black:false
+	bool color = false;
+	bool isCaptured = false;
 	Piece();
-	~Piece();
+	Piece(string name, bool newColor = false);
+	
 
 };
 
-Piece::Piece()
-{
-	isCaptured == false;
+Piece::Piece(string newName,bool newColor = false)
+{	
+	
+	if (newName == "Empty") {
+		name = newName;
+	}
+	else {
+		color = newColor;
+		name = newName;
+		isCaptured == false;
+	}
 }
 
-Piece::~Piece()
-{
-}
 
 
 
