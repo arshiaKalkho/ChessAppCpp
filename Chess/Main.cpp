65b1a6cdc51dca222621be12b175981f8b85abc6
arshia <arshiabdol@yahoo.com>
2021-09-09 06:24:33 -0400

    added loading screen, changed font size and cleared console for new operations

--- Chess/Main.cpp
+++ Chess/Main.cpp
@@ -1,20 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cwchar>
+#include <algorithm>
+
 
 
 #include"Board.h"
 #include "Piece.h"
 #include "Square.h"
 #include "windows.h"
-#include <algorithm>
+
 
 using namespace std;
 
 void initiate() {
+	//setting font and font size
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 0;                   
+	cfi.dwFontSize.Y = 30;                  
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	std::wcscpy(cfi.FaceName, L"Consolas"); 
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+
+	std::cout << "Font: Consolas, Size: 24\n";
 
 
 
-	HANDLE hconsole = GetStdHandle(STD_OUTPUT_HANDLE);//text color
-	
+
+
+	//changing text color
+	HANDLE hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hconsole, 10);//yellow
 
 	cout << "  cccccccccccccccc         hh               hh         eeeeeeeeeeeeeeeee           sssssssssssssss           sssssssssssssss   " << endl;
@@ -33,9 +51,18 @@ void initiate() {
 	cout << endl;
 
 	cout << "Coded by Arshia A Kalkhorani on September of 2021" << endl;
+	cout << "Loading";
+	Sleep(2000);
+	cout << ".";
+	Sleep(1000);
+	cout << ".";
+	Sleep(1500);
+	cout << ".";
 	cout << endl;
+	
+	
 
-	SetConsoleTextAttribute(hconsole, 15);//yellow
+	SetConsoleTextAttribute(hconsole, 15);
 }
 
 string rmWhiteSpaces(string text) {
@@ -43,12 +70,50 @@ string rmWhiteSpaces(string text) {
 	return text;
 }
 
+void clearConsole(){
+		
+	    //Get the handle to the current output buffer...
+		HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+		COORD coord = { 0, 0 };
+	
+		DWORD count;
+		
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+	
+		//Here we will set the current color
+		
+		if (GetConsoleScreenBufferInfo(hStdOut, &csbi))
+			
+		{
+			
+				//This fills the buffer with a given character (in this case 32=space).
+				
+				FillConsoleOutputCharacter(hStdOut, (TCHAR)32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+			
+				FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+			
+				//This will set our cursor position for the next print statement.
+				
+				SetConsoleCursorPosition(hStdOut, coord);
+			
+		}
+	
+		return;
+	
+}
+
+
 int main() {
 	
 	initiate();
+	
+	clearConsole();
 
 
 	Board board;
+	
+	
 	board.display();
 	
 	
